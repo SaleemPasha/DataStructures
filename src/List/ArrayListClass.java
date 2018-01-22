ae371551a84e343d4db3dd176bbdc9f8e23bae9f
ssp <sspasha93@gmail.com>
2018-01-21 22:42:12 -0600

    printing arraylist in many ways

--- src/List/ArrayListClass.java
+++ src/List/ArrayListClass.java
@@ -17,17 +17,28 @@ public class ArrayListClass {
 		numbers.add(6);
 		numbers.add(25);
 		numbers.add(01);
-		
+		//one way to print arraylist
 		System.out.println(numbers);
 		
 		
 		System.out.println("\n");
 		
+		numbers.remove(4);
+		//printing using Iterator
 		Iterator<Integer> p=numbers.iterator();
 		
 		while(p.hasNext())
 		System.out.println(p.next());
 		
+		System.out.println("\n");
+
+		//another way of printing
+		for(Integer x:numbers)
+			System.out.println(x);
+		
+		numbers.clear();
+		System.out.println("\n");
+		System.out.println(numbers.size()+" is the size of the arraylist");
 		
 	}
 }
