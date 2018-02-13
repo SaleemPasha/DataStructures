a570d73c9646e3ddfd5477a7f0773c8c073daa47
ssp <sspasha93@gmail.com>
2018-02-13 14:48:42 -0600

    LinkedHashSet

--- src/sets/HashSet.java
+++ src/sets/HashSet.java
@@ -1,7 +1,9 @@
 package sets;
 
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class HashSet {
 	 public static void main(String args[]){  
@@ -19,11 +21,32 @@ public class HashSet {
 		  while(itr.hasNext()){  
 		   System.out.println(itr.next());  
 		  }  
-		  
+		  System.out.println(seet.contains("Ravi"));
+			 
 		  seet.remove("Vijay");
-		  System.out.println("\nafter removing vijay: ");Iterator<String> it=seet.iterator();  
+		  System.out.println("\nafter removing vijay: ");
+		   Iterator<String> it=seet.iterator();  
 		  while(it.hasNext()){  
 		   System.out.println(it.next());  
-		  } 
+		   
+		  }
+		   
+		   java.util.HashSet<String> lhset = new java.util.HashSet<String>();
+
+	        // Adding elements to the LinkedHashSet
+	        lhset.add("Z");
+	        lhset.add("PQ");
+	        lhset.add("N");
+	        lhset.add("O");
+	        lhset.add("KK");
+	        lhset.add("FGH");
+	        System.out.println(lhset);
+	        
+	        System.out.println("converting hashset to treeset");
+	        
+	        Set<String> tset = new TreeSet<String>(lhset);
+	        
+	        System.out.println(tset);
+		  
 		 }  
 }
